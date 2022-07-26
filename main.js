a511db0e02dc99f5cab4b46284af6c1420887868
Priyam-Pramanik <86661996+Priyam-Pramanik@users.noreply.github.com>
2022-07-26 14:13:43 +0200

    Add files via upload
    A simple calculator,- init changes

---
+++ main.js
@@ -0,0 +1,26 @@
+function add(){
+    var result=
+    Number(document.getElementById("firstNo").value)+ Number(document.getElementById("secondNo").value);
+     window.alert(result);
+      
+    }
+    function sub(){
+    var result=
+     Number(document.getElementById("firstNo").value)-
+    Number(document.getElementById("secondNo").value);
+     window.alert(result); 
+    }
+    function multi(){
+    var result=
+     Number(document.getElementById("firstNo").value)*
+    Number(document.getElementById("secondNo").value);
+     window.alert(result); 
+      
+    }
+    function div(){
+    var result=
+     Number(document.getElementById("firstNo").value)/
+    Number(document.getElementById("secondNo").value);
+     window.alert(result); 
+    }
+    
